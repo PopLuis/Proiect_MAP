1f0a369b9ffd2f9f4435e728dae0b8fde565703e
PopLuis <144956647+PopLuis@users.noreply.github.com>
2024-12-03 22:13:34 +0000

    Inceput

--- Proiect_Map.c
+++ Proiect_Map.c
@@ -1,9 +1,24 @@
 #include<stdio.h>
 #include<string.h>
 
+typedef struct nod
+{
+    int valoare;
+    struct nod* stanga;
+    struct nod* dreapta;
+}Nod;
+
+Nod* creeazaNod(int val){
+    Nod* nod_N=(Nod*)malloc(sizeof(Nod));
+    nod_N->valoare=val;
+    nod_N->stanga=NULL;
+    nod_N->dreapta=NULL;
+    return nod_N;
+}
+
 int main(){
-    printf("Prima incercare!");
-    printf("GitHub");
+    Nod* radacina= NULL;
+    int val[]={};
 
 
 
